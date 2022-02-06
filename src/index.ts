bb565759c17c4be1023888e35b167972e08c652c
kyttd <kuyt1992@gmail.com>
2022-02-06 17:12:41 +0900

    feat: setting, try, checkメソッドを実行しゲームを実行できるようにした

--- src/index.ts
+++ src/index.ts
@@ -21,10 +21,59 @@ class HitAndBlow {
   answerSource = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   answer: string[] = [];
   tryCount = 0;
+
+  setting() {
+    const answerLength = 3;
+
+    while (this.answer.length < answerLength) {
+      const randNum = Math.floor(Math.random() * this.answerSource.length);
+      const selectedItem = this.answerSource[randNum];
+      if (!this.answer.includes(selectedItem)) {
+        this.answer.push(selectedItem);
+      }
+    }
+  }
+
+  async play() {
+    const inputArr = (
+      await promptInput("「,」区切りで3つの数字を入力してください")
+    ).split(",");
+    const result = this.check(inputArr);
+
+    if (result.hit !== this.answer.length) {
+      // 不正解だったら続行
+      printLine(`---\nHit: ${result.hit}\nBlow: ${result.blow}\n---`);
+      this.tryCount += 1;
+      await this.play();
+    } else {
+      // 正解だったら終了
+      this.tryCount += 1;
+    }
+  }
+
+  check(input: string[]) {
+    let hitCount = 0;
+    let blowCount = 0;
+
+    input.forEach((val, index) => {
+      if (val === this.answer[index]) {
+        hitCount += 1;
+      } else if (this.answer.includes(val)) {
+        blowCount += 1;
+      }
+    });
+
+    return {
+      hit: hitCount,
+      blow: blowCount,
+    };
+  }
 }
 
 process.stdout.write(sayHello("Michael Jackson"));
 
 (async () => {
   const hitAndBlow = new HitAndBlow();
+  hitAndBlow.setting();
+  await hitAndBlow.play();
 })();
