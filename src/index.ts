449d29fc4549ee1b984cd858542cd82edc9dc917
kyttd <kuyt1992@gmail.com>
2022-02-06 17:49:47 +0900

    feat: modeを導入(normal or hard)

--- src/index.ts
+++ src/index.ts
@@ -32,9 +32,23 @@ class HitAndBlow {
   ];
   private answer: string[] = [];
   private tryCount = 0;
+  private mode: "normal" | "hard";
+
+  constructor(mode: "normal" | "hard") {
+    this.mode = mode;
+  }
+
+  private getAnswerLength() {
+    switch (this.mode) {
+      case "normal":
+        return 3;
+      case "hard":
+        return 4;
+    }
+  }
 
   setting() {
-    const answerLength = 3;
+    const answerLength = this.getAnswerLength();
 
     while (this.answer.length < answerLength) {
       const randNum = Math.floor(Math.random() * this.answerSource.length);
@@ -46,8 +60,11 @@ class HitAndBlow {
   }
 
   async play() {
+    const answerLength = this.getAnswerLength();
     const inputArr = (
-      await promptInput("「,」区切りで3つの数字を入力してください")
+      await promptInput(
+        `「,」区切りで${answerLength}つの数字を入力してください`
+      )
     ).split(",");
 
     if (!this.validate(inputArr)) {
@@ -107,7 +124,7 @@ class HitAndBlow {
 process.stdout.write(sayHello("Michael Jackson"));
 
 (async () => {
-  const hitAndBlow = new HitAndBlow();
+  const hitAndBlow = new HitAndBlow("hard");
   hitAndBlow.setting();
   await hitAndBlow.play();
   hitAndBlow.end();
