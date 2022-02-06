f5f194bc1c77e28074657ee2f91ac772a0116679
kyttd <kuyt1992@gmail.com>
2022-02-06 14:48:55 +0900

    feat: 対話用のコマンドを実装

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,27 @@ const sayHello = (name: string) => {
   return `Hello, ${name}!`;
 };
 
+// 受け取った値を出力
+const printLine = (text: string, breakline: boolean = true) => {
+  process.stdout.write(text + (breakline ? "\n" : ""));
+};
+
+// 対話用の関数
+const promptInput = async (text: string) => {
+  printLine(`\n${text}\n> `, false);
+  const input: string = await new Promise((resolve) =>
+    process.stdin.once("data", (data) => resolve(data.toString()))
+  );
+
+  return input.trim();
+};
+
 process.stdout.write(sayHello("Michael Jackson"));
+
+(async () => {
+  const name = await promptInput("名前を入力してください");
+  console.log(name);
+  const age = await promptInput("年齢を入力してください");
+  console.log(age);
+  process.exit();
+})();
