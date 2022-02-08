4442b5551897ca440ca906bdc9d43d934e7aa6b3
kyttd <kuyt1992@gmail.com>
2022-02-08 23:13:49 +0900

    refact: Mode型のリファクタ(as const, typeof)

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,8 @@ const promptSelect = async <T extends string>(
   }
 };
 
-type Mode = "normal" | "hard";
+const modes = ["normal", "hard"] as const;
+type Mode = typeof modes[number];
 
 class HitAndBlow {
   private readonly answerSource = [
@@ -72,10 +73,7 @@ class HitAndBlow {
   }
 
   async setting() {
-    this.mode = await promptSelect<Mode>("モードを入力してください。", [
-      "normal",
-      "hard",
-    ]);
+    this.mode = await promptSelect<Mode>("モードを入力してください。", modes);
     const answerLength = this.getAnswerLength();
 
     while (this.answer.length < answerLength) {
